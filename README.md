7c315877f148ac1f58d7325470bda3854fa04de5
sun17 <m15829090357@163.com>
2021-01-13 16:11:09 +0800

    readme.md编写

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 #### 目录介绍
-
+    application 应用层 该层用作域和界面层之间的通道。将请求从接口层发送到域层，由域层处理请求并返回响应
+     
     domain 领域层
         aggregates 聚合
 		entity 实体
 		po 与数据库交互
 		valueobj 值对象
 		vo  View Object
+	
+	infra Infrastructure基础设施层
+	    config 项目相关配置
+	    util 主要放数据库、缓存、文件、网关等第三方类库
+	
+	interfaces 该层包含与其他系统交互的所有内容，例如Web服务
+	    assembler 组装 实现DTO 与领域对象之间的相互转换、映射
+	    dto 数据传输的载体，我们可以通过DTO把内部的领域对象与外界隔离
+	    
+	server 
+	    main.go 项目运行脚本
+	    
+	
--- domain/repos/UserInterface.go
+++ domain/repos/UserInterface.go
@@ -2,14 +2,14 @@ package repos
 
 import "github.com/sk532359025/DDD/domain/entity"
 
-type IUserRepos interface {
+type UserInterface interface {
 	FindByName(name string) *entity.UserEntity
 	SaveUser(use *entity.UserEntity) *entity.UserEntity
 	UpdateUser(id int) error
 	DeleteUser(id int) error
 }
 
-type IUserLogRepos interface {
+type UserLogInterface interface {
 	FindByUserId(UserId int) *entity.UserLogEntity
 	SaveUser(log *entity.UserLogEntity) *entity.UserLogEntity
 }
